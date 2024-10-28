5fb46cb0d9d356a53bf5ab2370b0d170136d5930
Mihail Komarov <michaelkomarov03@gmail.com>
2024-10-28 16:51:06 +0200

    min-stack: pass

--- lab1/Stack/min-stack/MinStack.py
+++ lab1/Stack/min-stack/MinStack.py
@@ -13,6 +13,7 @@ class MyLinkedList:
             self.first_element = first_element
             self.length = 1
         else:
+            self.first_element = None
             self.length = 0
 
     def get(self, index) -> int:
@@ -92,6 +93,14 @@ class MyLinkedList:
         if current_element is not None and current_element.next_element is not None:
             current_element.next_element = current_element.next_element.next_element
             self.length -= 1
+    def __str__(self):
+        output = "LinkedList {"
+        current_element = self.first_element
+        while current_element is not None:
+            output += f"{current_element.value},"
+            current_element = current_element.next_element
+        output += "}"
+        return output
 
 
 class MinStack:
@@ -101,11 +110,19 @@ class MinStack:
         self.min_stack = MyLinkedList()
 
     def push(self, val: int) -> None:
+        # print(f"Pushing {val}")
         # add element to the start of main LL
         self.main_stack.addAtHead(val)
+
         # add element to the start of min LL if it is less than the current min
-        if val <= self.min_stack.get(0):
+        if self.min_stack.length == 0 or val <= self.min_stack.get(0):
+            # print(f"Adding {val} to min stack.")
             self.min_stack.addAtHead(val)
+
+
+        # print("Pushed.")
+        # print(f"Main stack: {self.main_stack}")
+        # print(f"Min stack: {self.min_stack}")
 
     def pop(self) -> None:
         # remove first element from min LL if it's value is the same as the first element of main LL
@@ -113,10 +130,13 @@ class MinStack:
             self.min_stack.deleteAtIndex(0)
         # remove first element from main LL
         self.main_stack.deleteAtIndex(0)
+
     def top(self) -> int:
+        # return the first element of main LL
         return self.main_stack.get(0)
-    # return the first element of main LL
+
     def getMin(self) -> int:
+        # return the first element of min LL
         return self.min_stack.get(0)
 
 # Your MinStack object will be instantiated and called as such:
