a4fa18ee248fe08ad067cdafb88df1f0aaeed797
Mihail Komarov <michaelkomarov03@gmail.com>
2024-10-28 13:49:34 +0200

    min-stack: in progress

---
+++ lab1/Stack/min-stack/MinStack.py
@@ -0,0 +1,127 @@
+from typing import Optional
+
+
+class MyLinkedListNode:
+    def __init__(self, val: int, next_element: Optional["MyLinkedListNode"] = None):
+        self.value = val
+        self.next_element = next_element
+
+
+class MyLinkedList:
+    def __init__(self, first_element: Optional["MyLinkedListNode"] = None):
+        if first_element is not None:
+            self.first_element = first_element
+            self.length = 1
+        else:
+            self.length = 0
+
+    def get(self, index) -> int:
+        if index >= self.length:
+            return -1
+
+        element_counter = 0
+        current_element = self.first_element
+        while element_counter != index:
+            current_element = current_element.next_element
+            element_counter += 1
+
+        return current_element.value
+
+    def addAtHead(self, val) -> None:
+        if self.length == 0:
+            self.first_element = MyLinkedListNode(val)
+            self.length += 1
+            return
+
+        new_element = MyLinkedListNode(val)
+        new_element.next_element = self.first_element
+        self.first_element = new_element
+        self.length += 1
+
+    def addAtTail(self, val) -> None:
+        new_element = MyLinkedListNode(val)
+
+        if self.length == 0:
+            self.first_element = new_element
+            self.length += 1
+            return
+
+        current_element = self.first_element
+        while current_element.next_element is not None:
+            current_element = current_element.next_element
+
+        current_element.next_element = new_element
+        self.length += 1
+
+    def addAtIndex(self, index, val) -> None:
+        if index > self.length:
+            return
+        if index == self.length:
+            self.addAtTail(val)
+            return
+        if index == 0:
+            self.addAtHead(val)
+            return
+
+        element_counter = 0
+        current_element = self.first_element
+        while element_counter + 1 != index:
+            current_element = current_element.next_element
+            element_counter += 1
+
+        new_element = MyLinkedListNode(val)
+        new_element.next_element = current_element.next_element
+        current_element.next_element = new_element
+        self.length += 1
+
+    def deleteAtIndex(self, index) -> None:
+        if index >= self.length or index < 0:
+            return
+
+        if index == 0:
+            self.first_element = self.first_element.next_element
+            self.length -= 1
+            return
+
+        element_counter = 0
+        current_element = self.first_element
+        while element_counter + 1 != index:
+            current_element = current_element.next_element
+            element_counter += 1
+
+        if current_element is not None and current_element.next_element is not None:
+            current_element.next_element = current_element.next_element.next_element
+            self.length -= 1
+
+
+class MinStack:
+
+    def __init__(self):
+        self.main_stack = MyLinkedList()
+        self.min_stack = MyLinkedList()
+
+    def push(self, val: int) -> None:
+        # add element to the start of main LL
+        self.main_stack.addAtHead(val)
+        # add element to the start of min LL if it is less than the current min
+        if val <= self.min_stack.get(0):
+            self.min_stack.addAtHead(val)
+
+    def pop(self) -> None:
+        # remove first element from min LL if it's value is the same as the first element of main LL
+        if self.main_stack.get(0) == self.min_stack.get(0):
+            self.min_stack.deleteAtIndex(0)
+        # remove first element from main LL
+        self.main_stack.deleteAtIndex(0)
+    def top(self) -> int:
+        return self.main_stack.get(0)
+    # return the first element of main LL
+    def getMin(self) -> int:
+        return self.min_stack.get(0)
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
