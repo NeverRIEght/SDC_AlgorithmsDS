82bf943a2c08a391b5b979bce07f211f5018a550
Mihail Komarov <michaelkomarov03@gmail.com>
2024-11-25 16:57:56 +0200

    number-of-provinces

--- lab3/number-of-provinces/main.py
+++ lab3/number-of-provinces/main.py
@@ -1,53 +1,45 @@
 from collections import deque
 
 
-def bfs(first_elem):
-    q = deque()
-    q.append(first_elem)
+# def bfs(first_elem):
+#     q = deque()
+#     q.append(first_elem)
+#
+#     while q:
+#         current_poll = q.popleft()
+#         print(current_poll)
+#         for item in current_poll:
+#             q.append(item)
 
-    while q:
-        current_poll = q.popleft()
-        print(current_poll)
-        for item in current_poll:
-            q.append(item)
-        else:
-            pass
+# def dfs(first_elem):
+#     q = deque()
+#     q.append(first_elem)
+#
+#     while q:
+#         current_poll = q.pop()
+#         print(current_poll)
+#         for item in current_poll:
+#             q.append(item)
 
-def dfs(first_elem):
-    q = deque()
-    q.append(first_elem)
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        circles_count = len(isConnected)
+        visited = set()
 
-    while q:
-        current_poll = q.pop()
-        print(current_poll)
-        for item in current_poll:
-            q.append(item)
-        else:
-            pass
+        connected_components = 0
 
+        for i in range(circles_count):
+            q = deque()
+            if i not in visited:
+                visited.add(i)
+                q.append(i)
+                connected_components += 1
+            while q:
+                current_node = q.popleft()
+                for neighbour in range(circles_count):
+                    if neighbour not in visited and neighbour != current_node:
+                        if isConnected[current_node][neighbour] == 1:
+                            visited.add(neighbour)
+                            q.append(neighbour)
 
-def main():
-    is_connected = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
-    n = len(is_connected)
-    visited = set()
-
-    connected_components = 0
-
-    for i in range(n):
-        q = deque()
-        if i not in visited:
-            visited.add(i)
-            q.append(i)
-            connected_components += 1
-        while q:
-            current_node = q.popleft()
-            for adj in range(n):
-                if adj not in visited:
-                    if adj != current_node:
-                        if is_connected[current_node][adj] == 1:
-                            visited.add(adj)
-                            q.append(adj)
-
-    print(connected_components)
-
-main()
+        return connected_components
