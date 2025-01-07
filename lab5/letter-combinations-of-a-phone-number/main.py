330c638c99fc2a6f3a3fbf772e4c1541f367c0aa
Mihail Komarov <michaelkomarov03@gmail.com>
2025-01-07 12:57:21 +0200

    letter-combinations-of-a-phone-number

---
+++ lab5/letter-combinations-of-a-phone-number/main.py
@@ -0,0 +1,28 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if digits is None or len(digits) == 0:
+            return []
+
+        digit_to_char = {
+            '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
+            '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'
+        }
+
+        # Queue for all possible combinations
+        combinations_queue = deque()
+
+        # Add all characters for the first digit
+        for char in digit_to_char[digits[0]]:
+            combinations_queue.append(char)
+
+        # For each digit, except the first
+        for digit in digits[1:]:
+            # For each combination
+            for _ in range(len(combinations_queue)):
+                # Delete the current combination and append it with all possible characters
+                current_combination = combinations_queue.popleft()
+                for char in digit_to_char[digit]:
+                    combinations_queue.append(current_combination + char)
+
+        # Return queue as list
+        return list(combinations_queue)
