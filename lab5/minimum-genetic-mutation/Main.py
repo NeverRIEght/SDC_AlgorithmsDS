ab0891a4422c7093ef0b10dc7260f5bc49ca4241
Mihail Komarov <michaelkomarov03@gmail.com>
2025-01-07 15:58:48 +0200

    minimum-genetic-mutation

---
+++ lab5/minimum-genetic-mutation/Main.py
@@ -0,0 +1,40 @@
+class Solution:
+    def minMutation(self, startGene: str, endGene: str, bank: list) -> int:
+        # BFS
+        # O(4 * L * N)
+        # L - length of the gene
+        # N - the number of genes in the bank
+
+        if endGene not in bank:
+            return -1
+
+        gene_symbols = ['A', 'C', 'G', 'T']
+
+        # Deque-based BFS (startGene, mutations counter)
+        queue = deque([(startGene, 0)])
+
+        visited = set([startGene])
+
+        while queue:
+            current_gene, mutations = queue.popleft()
+
+            # Destination reached
+            if current_gene == endGene:
+                return mutations
+
+            # Генерируем все возможные мутации
+            for i in range(len(current_gene)):
+                for current_symbol in gene_symbols:
+                    # Do not override the same symbol
+                    if current_gene[i] == current_symbol:
+                        continue
+
+                    # New mutation
+                    mutated_gene = current_gene[:i] + current_symbol + current_gene[i + 1:]
+
+                    if mutated_gene in bank and mutated_gene not in visited:
+                        visited.add(mutated_gene)
+                        queue.append((mutated_gene, mutations + 1))
+
+        # No mutations found
+        return -1
